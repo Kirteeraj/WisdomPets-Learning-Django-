ebeb89b7a7c7c4c165006559595bb8421179ac27
Kirteeraj Malkar <kirteerajmalkar@gmail.com>
2020-05-17 13:10:10 +0530

    upto implementings urls

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models herefrom
+from .models import Pet
+
+@admin.register(Pet)
+
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'species','breed','age','sex']
